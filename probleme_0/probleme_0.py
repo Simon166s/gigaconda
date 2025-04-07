a07ea85f442f80c249e5f0a31d628d35899036b2
leduclean <leandre.leduc38@gmail.com>
2025-04-07 15:16:52 +0200

    calcul pour maximiser la taille et non la durée

--- probleme_0/probleme_0.py
+++ probleme_0/probleme_0.py
@@ -1,21 +1,8 @@
 Hmin = 0
 Hmax = 10E9
 
-# ? est ce qu ón doit faire ca 
-# ! Attention
-# TODO change implementation 
-
-def duree_planning(planning):
-    if not(planning):
-        return float("-inf")
-    total = 0
-    for k in planning :
-        (debut,fin) = k
-        total += fin-debut
- 
-    return total
         
-def valide(creneau_test, curr):
+def valide(creneau_test: tuple, curr: list) -> bool:
     
     debut_test, fin_test = creneau_test
     if creneau_test in curr :
@@ -27,28 +14,28 @@ def valide(creneau_test, curr):
         
     return True
         
-def optim_planning(demandes, curr = []):
+def optim_planning(demandes: list, curr: list = []) -> tuple:
     
-    demandes = list(demandes)
-    
-    plannings=[]
+    demandes = demandes
+
+    plannings = []
     a = 0
     for creneau in demandes :
         if valide(creneau,curr)  :
             a +=1
             curr.append(creneau)
-            plannings.append(optim_planning(demandes,curr))
+            plannings.append(optim_planning(demandes, curr))
             curr.pop()
             
     if a == 0 :
         return curr.copy() # on a pas de nouvelle maniere
     if plannings :
-        return max(plannings, key=duree_planning)
+        return max(plannings, key=len)
     
     else :
         print("test")
         return None
 
-exemple_demandes = ((2,5),(7,9),(3,9),(2,6),(4,7))
+exemple_demandes = [(2,5),(7,9),(3,9),(2,6),(4,7)]
 
 print(optim_planning(exemple_demandes))
