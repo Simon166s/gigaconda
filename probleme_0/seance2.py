547fd182923b8b7526dcc7459e3f3d7916ed2427
alban604 <alban.derycke@gmail.com>
2025-04-09 10:18:40 +0200

    version 09/04 10:17

--- probleme_0/seance2.py
+++ probleme_0/seance2.py
@@ -11,7 +11,7 @@ def valide(creneau_test, curr):
         
     return True
 
-class valideur:
+"""class valideur:
     def __init__(self,creneau_min,creneau_max):
         #print(creneau_max)
         self.agenda = np.array([0 for a in range(0,creneau_max)])
@@ -23,20 +23,17 @@ class valideur:
     def maj(self,creneau):
         hd,hf=creneau
         #print(hd,hf)
-        self.agenda[hd:hf]=1
-        
+        self.agenda[hd:hf]=1"""
+ 
 
 def optim_planning(demandes):
     demande_trie = sorted(demandes,key=lambda x:x[1],reverse=True)
     #print(demande_trie)
-    liste_result = []    
-    V = valideur(min(demandes,key= lambda x:x[0])[0],max(demandes,key= lambda x:x[1])[1])
+    liste_result = [demande_trie.pop()]
     for i in range(len(demande_trie)):
         meilleur_plage = demande_trie.pop()
-        if V.get_maj_possible(meilleur_plage):
+        if liste_result[-1][1]<=meilleur_plage[0]:
             liste_result.append(meilleur_plage)
-            V.maj(meilleur_plage)
-    print(V.agenda)
     return sorted(liste_result,key=lambda x:x[0])
 
 exemple_demandes = ((2,5),(7,9),(3,9),(2,6),(4,7))
