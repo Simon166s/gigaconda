4b59ee98ff24ebf048a7ff3ff04f399c92835210
alban604 <alban.derycke@gmail.com>
2025-04-09 10:08:45 +0200

    version finale 0904

--- probleme_0/seance2.py
+++ probleme_0/seance2.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 def valide(creneau_test, curr):
     
     debut_test, fin_test = creneau_test
@@ -11,14 +11,32 @@ def valide(creneau_test, curr):
         
     return True
 
+class valideur:
+    def __init__(self,creneau_min,creneau_max):
+        #print(creneau_max)
+        self.agenda = np.array([0 for a in range(0,creneau_max)])
+    def get_maj_possible(self,creneau):
+        hd,hf=creneau
+        if 1 not in self.agenda[hd:hf]:
+            return True
+
+    def maj(self,creneau):
+        hd,hf=creneau
+        #print(hd,hf)
+        self.agenda[hd:hf]=1
+        
+
 def optim_planning(demandes):
-    demande_trie = sorted(demandes,key=lambda x:x[1]-x[0],reverse=True)
-    print(demande_trie)
+    demande_trie = sorted(demandes,key=lambda x:x[1],reverse=True)
+    #print(demande_trie)
     liste_result = []    
+    V = valideur(min(demandes,key= lambda x:x[0])[0],max(demandes,key= lambda x:x[1])[1])
     for i in range(len(demande_trie)):
         meilleur_plage = demande_trie.pop()
-        if valide(meilleur_plage,liste_result):
+        if V.get_maj_possible(meilleur_plage):
             liste_result.append(meilleur_plage)
+            V.maj(meilleur_plage)
+    print(V.agenda)
     return sorted(liste_result,key=lambda x:x[0])
 
 exemple_demandes = ((2,5),(7,9),(3,9),(2,6),(4,7))
