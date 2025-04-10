2a32598fbe31f8926585d60988e4a5bc87db350e
Simon <simonvanbommel@gmail.com>
2025-04-10 15:12:29 +0200

    Initialisation Probleme_1

---
+++ probleme_1/main.py
@@ -0,0 +1,10 @@
+# Code de base pour le problème `Tournée du jardinier`
+
+import util
+
+def calcule_tournee(coords):
+    return coords # A MODIFIER
+
+coords = util.lire_fichier_coords('exemple1.txt')
+tournee = calcule_tournee(coords)
+util.affiche_tournee(tournee)
