f11b108e5dc9807ac9733889de1826ccbde80665
Simon <simonvanbommel@gmail.com>
2025-04-14 15:14:19 +0200

    fichier test pour les differentes fonctions

---
+++ probleme_1/test_simon.py
@@ -0,0 +1,17 @@
+import main
+import affichage
+import util
+import itertools
+coords = util.lire_fichier_coords('exemple1.txt')
+coords2 = util.lire_fichier_coords('exemple2.txt')
+
+# util.affiche_points(coords)
+# tournee = main.calcul_tournee_ex(list(coords))
+# util.affiche_tournee(tournee)
+
+# util.affiche_points(coords2)
+# tournee2= main.calcul_tournee_ex(list(coords2))
+# util.affiche_tournee(tournee2)
+
+meilleure_tournee = min(itertools.permutations(coords), key=util.distance_totale)
+util.affiche_tournee(meilleure_tournee)
