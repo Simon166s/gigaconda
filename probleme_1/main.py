a4a5395add9d6e7d3ba1e448925cd1c71082a0f8
Simon <simonvanbommel@gmail.com>
2025-04-14 15:20:13 +0200

    reorganisation de main.py

--- probleme_1/main.py
+++ probleme_1/main.py
@@ -1,35 +1,17 @@
 # Code de base pour le problème `Tournée du jardinier`
 import matplotlib.pyplot as plt
-import util
+from util import distance_totale, lire_fichier_coords, affiche_points, affiche_tournee
 
-def calcule_tournee(coords):
-    return coords # A MODIFIER
-
-coords = util.lire_fichier_coords('test_alban_etoile.txt')
-tournee = calcule_tournee(coords)
-#util.affiche_tournee(tournee)
-
-#util.affiche_points(coords)
+coords = lire_fichier_coords('test_alban_etoile.txt')
 
 distance_min = float("+inf")
-
-def distance(p1, p2):
-    return ( (p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 )**(1/2)
-
-def distance_totale(coords):
-    if len(coords) == 0:
-        return 0
-    dist = distance((0, 0), coords[0]) + distance(coords[-1], (0, 0))
-    for i in range(1, len(coords)):
-        dist += distance(coords[i], coords[i-1])
-    return dist
 
 meilleur_chemins = []
 taille_min_chem =  float("+inf")
 
 def calcul_tournee_ex(coords, curr=[]):
     global taille_min_chem
-    d_tot = util.distance_totale(curr)
+    d_tot = distance_totale(curr)
     if taille_min_chem <= d_tot:
         return 
     if len(coords) == 0:
@@ -60,5 +42,5 @@ def appel_cacul_tournee(coords):
 tournee = appel_cacul_tournee(list(coords))
 #print(tournee)
 
-util.affiche_points(coords)
-util.affiche_tournee(tournee)
+affiche_points(coords)
+affiche_tournee(tournee)
