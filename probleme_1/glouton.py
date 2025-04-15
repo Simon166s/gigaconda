80ec90589d863370b75ef894a3d928a252f3e2ae
leduclean <leandre.leduc38@gmail.com>
2025-04-15 14:12:47 +0200

    creation d'heuristiques sur les arcs aléatoires

--- probleme_1/glouton.py
+++ probleme_1/glouton.py
@@ -3,6 +3,7 @@ from main import appel_cacul_tournee
 import random
 
 
+
 def glouton(coords: np.array):
     current_pt = (0, 0)
     current_solution = [current_pt]
@@ -39,40 +40,96 @@ def local_heuristic_k_pt(solution_glouton: list, k: int):
 
     return current_better
 
-# De base j'ai fait ca mais sans volonté d'amélioration donc en fait ca revient a une fonction voisin 
-# ? Plutot voisin que heuristique locale si on enleve la condition d'amelioration 
 
+
+previous_points = (-1, -1)
 def echanger_arcs(solution):
-    # Crée une copie de la solution avant de modifier
-    solution_copy = solution[:]
-    i, j = random.sample(range(1, len(solution_copy) - 1), 2)  # éviter le point de départ (0, 0)
-    solution_copy[i], solution_copy[j] = solution_copy[j], solution_copy[i]
-    return solution_copy
+    global previous_points
+    i, j = random.sample(range(1, len(solution) - 1), 2)  # éviter le point de départ (0, 0)
+    if (i,j) != previous_points:
+        solution_copy = solution[:]
+        solution_copy[i], solution_copy[j] = solution_copy[j], solution_copy[i]
+        previous_points = (i,j)
+        return solution_copy
+    return solution
 
 
-def local_heuristic_arc(solution_glouton: list, max_iter= 50):
+def local_heuristic_arc(solution_glouton: list, max_stagnation: int = 10000):
+    sortir = False 
+    nbr_meilleur = 0
+    stagnation = 0
     current_better = solution_glouton
-    itr = 1
-    while itr <= max_iter:
+    while not sortir:
+
         potential_better_solution = echanger_arcs(current_better)
-        current_better = (
-            current_better
-            if distance_totale(current_better)
-            < distance_totale(potential_better_solution)
-            else potential_better_solution
-        )
-        itr += 1 
+        if distance_totale(current_better) > distance_totale(potential_better_solution):
+            current_better = potential_better_solution
+            nbr_meilleur += 1
+        else:
+            stagnation += 1
+
+        sortir = True if nbr_meilleur >= 100000 or stagnation >= max_stagnation else False 
+
     return current_better
 
-# ? Idée echanger deux arcs jusqu'a ce que ce soit mieux ie prendre deux arcs et inverser leur point d'arriver ? 
+
+
+
+previous = None  # à initialiser avant d'appeler la fonction
+
+def echanger_arcs_k_consec(solution: list, k: int):
+    global previous
+    i = random.randint(1, len(solution) - 2)
+    if i != previous:
+        previous = i
+        start = max(i - k // 2, 1)
+        end = min(i + k // 2 + 1, len(solution) - 1)
+        solution_copy = solution[:]
+        slice_indices = range(start, end)
+        slice_values = [solution_copy[j] for j in slice_indices]
+        random.shuffle(slice_values)
+
+        for idx, val in zip(slice_indices, slice_values):
+            solution_copy[idx] = val
+
+        return solution_copy
+    return solution
+
+
+def local_heuristic_arc_consec(solution: list, k: int, max_stagnation: int = 10000):
+    sortir = False 
+    nbr_meilleur = 0
+    stagnation = 0
+    current_better = solution
+    while not sortir:
+
+        potential_better_solution = echanger_arcs_k_consec(current_better, k)
+        if distance_totale(current_better) > distance_totale(potential_better_solution):
+            current_better = potential_better_solution
+            nbr_meilleur += 1
+        else:
+            stagnation += 1
+
+        sortir = True if nbr_meilleur >= 100000 or stagnation >= max_stagnation else False 
+
+    return current_better
+
+
+
 
 coords = lire_fichier_coords("exemple2.txt")
 solution_init = glouton(coords)
 print(distance_totale(solution_init))
 
-heuristic_k_pt = local_heuristic_k_pt(solution_init, 7)
+
+local_heuristic_arc = local_heuristic_arc_consec(solution_init, 2)
+print(distance_totale(local_heuristic_arc))
+
+heuristic_k_pt = local_heuristic_k_pt(local_heuristic_arc, 6)
 print(distance_totale(heuristic_k_pt))
 
-local_heuristic_arc = local_heuristic_arc(solution_init, 1000000)
-print(distance_totale(local_heuristic_arc))
+
+
+# local_heuristic_arc = local_heuristic_arc(heuristic_k_pt)
+# print(distance_totale(local_heuristic_arc))
 
