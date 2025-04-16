2562b1f4b819114cf196674f2d7861be76821e43
Simon <simonvanbommel@gmail.com>
2025-04-16 14:00:02 +0200

    recherche exhaustive fonctionne seulement avec np.array

--- probleme_1/main.py
+++ probleme_1/main.py
@@ -3,49 +3,49 @@ import matplotlib.pyplot as plt
 from util import distance_totale, lire_fichier_coords, affiche_points, affiche_tournee
 import numpy as np
 
-# coords = lire_fichier_coords('test_alban_etoile.txt')
+coords = lire_fichier_coords('exemple1.txt')
 
 distance_min = float("+inf")
 
-meilleur_chemins = []
+meilleur_chemins = np.array([])
 taille_min_chem = float("+inf")
 
 
-def calcul_tournee_ex(coords, curr=[[0, 0]]):
-    global taille_min_chem
+def calcul_tournee_ex(coords, curr = np.array([[0, 0]], dtype=float)):
+    global taille_min_chem, meilleur_chemins
     if taille_min_chem <= distance_totale(curr):
         return
+    
     if len(coords) == 0:
-        global meilleur_chemins
-        d_tot = distance_totale(curr + [[0, 0]])
+        
+        d_tot = distance_totale(np.vstack([curr, [0, 0]]))
         if d_tot < taille_min_chem:
             taille_min_chem = d_tot
-            meilleur_chemins = curr[:] + [[0, 0]]
+            meilleur_chemins = np.vstack([curr, [0, 0]])
         return
 
     for i in range(len(coords)):
-        point = coords.pop(i)
-        curr.append(point)
-
-        calcul_tournee_ex(coords, curr)  # appel récursif
-
-        curr.pop()
-        coords.insert(i, point)  # on remet le point à sa place d'origine
+        point = coords[i]
+        new_coords = np.delete(coords, i, axis=0)
+        new_curr = np.vstack([curr, point])
+        
+        calcul_tournee_ex(new_coords, new_curr)
 
 
 def appel_cacul_tournee(coords):
-    global meilleur_chemins
-    global taille_min_chem
-    meilleur_chemins = []
+    global meilleur_chemins, taille_min_chem
+    meilleur_chemins = np.empty((0, 2))
     taille_min_chem = float("+inf")
-    calcul_tournee_ex(coords)
+    
+    # Convertir les coordonnées en numpy array si ce n'est pas déjà le cas
+    coords_array = np.array(coords, dtype=float)
+    
+    calcul_tournee_ex(coords_array)
     return meilleur_chemins
 
 
-print(appel_cacul_tournee)
-
-# tournee = appel_cacul_tournee(list(coords))
-# #print(tournee)
+# tournee = appel_cacul_tournee(coords)
+# print(tournee)
 
 # affiche_points(coords)
 # affiche_tournee(tournee)
