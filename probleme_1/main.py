f0a85b761be26ad96b8892d98e3ee0caed5fbcd4
alban604 <alban.derycke@gmail.com>
2025-04-16 15:22:29 +0200

    amelioration de la complexite de la fonction exhaustive

--- probleme_1/main.py
+++ probleme_1/main.py
@@ -1,6 +1,6 @@
 # Code de base pour le problème `Tournée du jardinier`
 import matplotlib.pyplot as plt
-from util import distance_totale, lire_fichier_coords, affiche_points, affiche_tournee
+from util import distance_totale, lire_fichier_coords, affiche_points, affiche_tournee,distance
 import numpy as np
 
 coords = lire_fichier_coords('exemple1.txt')
@@ -11,14 +11,14 @@ meilleur_chemins = np.array([])
 taille_min_chem = float("+inf")
 
 
-def calcul_tournee_ex(coords, curr = np.array([[0, 0]], dtype=float)):
+def calcul_tournee_ex(coords, curr = np.array([[0, 0]], dtype=float),long_actuel=0):
     global taille_min_chem, meilleur_chemins
-    if taille_min_chem <= distance_totale(curr):
+    if taille_min_chem <= long_actuel:
         return
     
     if len(coords) == 0:
         
-        d_tot = distance_totale(np.vstack([curr, [0, 0]]))
+        d_tot = long_actuel+distance(curr[-1],[0,0])
         if d_tot < taille_min_chem:
             taille_min_chem = d_tot
             meilleur_chemins = np.vstack([curr, [0, 0]])
@@ -29,7 +29,7 @@ def calcul_tournee_ex(coords, curr = np.array([[0, 0]], dtype=float)):
         new_coords = np.delete(coords, i, axis=0)
         new_curr = np.vstack([curr, point])
         
-        calcul_tournee_ex(new_coords, new_curr)
+        calcul_tournee_ex(new_coords, new_curr,long_actuel+distance(new_curr[-1],new_curr[-2]))
 
 
 def appel_calcul_tournee(coords):
@@ -44,8 +44,8 @@ def appel_calcul_tournee(coords):
     return meilleur_chemins
 
 
-# tournee = appel_calcul_tournee(coords)
-# print(tournee)
+tournee = appel_calcul_tournee(coords)
+#print(tournee)
 
 # affiche_points(coords)
 # affiche_tournee(tournee)
