af3bdaab62f1baf317c17c533913a39613753e08
leduclean <leandre.leduc38@gmail.com>
2025-04-24 16:02:39 +0200

    meilleurs commentaires et variables

--- probleme_2/recu.py
+++ probleme_2/recu.py
@@ -19,7 +19,7 @@ def aux(
     longueurs_mots: np.array,
     memo: dict,
     coupures: dict,
-    indice_mot_courant: int,
+    i: int,
     largeur: int = 80,
 ) -> int:
     """
@@ -29,44 +29,44 @@ def aux(
         longueurs_mots (np.array): longueurs de chaque mot du texte
         memo (dict): memoïsation des coûts déjà calculés
         coupures (dict): enregistrements des meilleures positions de coupure
-        indice_mot_courant (int): nombre de mots considérés
+        i (int): nombre de mots considérés
         largeur (int): largeur maximale d'une ligne
 
     Returns:
-        int: coût minimal pour justifier les mots jusqu'à indice_mot_courant
+        int: coût minimal pour justifier les mots jusqu'au `i`-ème mot
     """
     # Cas de base
-    if indice_mot_courant == 0:
+    if i == 0:
         memo[0] = 0
         return 0
 
     # Renvoi si déjà calculé
-    if indice_mot_courant in memo:
-        return memo[indice_mot_courant]
+    if i in memo:
+        return memo[i]
 
     cout_minimal = float("inf")
     meilleure_coupure = 0
 
     # Tester chaque position de début de ligne possible de manière décroissante
-    for indice_debut in range(indice_mot_courant - 1, -1, -1):
-        longueur_ligne = sum(longueurs_mots[indice_debut:indice_mot_courant]) + (
-            indice_mot_courant - indice_debut - 1
+    for j in range(i - 1, -1, -1):
+        longueur_ligne = sum(longueurs_mots[j:i]) + (
+            i - j - 1
         )
 
-        # Comme on parcour de manière décroissante, des qu'on dépasse la largeur de la ligne, on est sur que les itérations suivantes seront non aussi supérieures à la largeur
+        # Comme on parcours de manière décroissante, des qu'on dépasse la largeur de la ligne, on est sur que les itérations suivantes seront non aussi supérieures à la largeur
         if longueur_ligne > largeur:
             break
         # coût induit par l'espace restant carré
         cout_espace = (largeur - longueur_ligne) ** 2
         cout_total = (
-            aux(longueurs_mots, memo, coupures, indice_debut, largeur) + cout_espace
+            aux(longueurs_mots, memo, coupures, j, largeur) + cout_espace
         )
         if cout_total < cout_minimal:
             cout_minimal = cout_total
-            meilleure_coupure = indice_debut
+            meilleure_coupure = j
 
-    memo[indice_mot_courant] = cout_minimal
-    coupures[indice_mot_courant] = meilleure_coupure
+    memo[i] = cout_minimal
+    coupures[i] = meilleure_coupure
     return cout_minimal
 
 
@@ -114,9 +114,9 @@ def justifier_iteratif(
     """
 
     n = len(longueurs_mots)
-    dp = [float("inf")] * (n + 1)
+    liste_des_couts = [float("inf")] * (n + 1)
     coupures = {}
-    dp[0] = 0
+    liste_des_couts[0] = 0
 
     # Pour chaque nouveau mot à insérer
     for i in range(1, n + 1):
@@ -128,13 +128,13 @@ def justifier_iteratif(
             if longueur_ligne > largeur:
                 break
             # On calcule le cout totale avec cette nouvelle solution potentielle, on utilise les couts des sous problèmes précédents
-            cout = dp[j] + (largeur - longueur_ligne) ** 2
+            cout = liste_des_couts[j] + (largeur - longueur_ligne) ** 2
 
             # On garde le meilleur cout pour le `i`-eme mot
-            if cout < dp[i]:
-                dp[i] = cout
+            if cout < liste_des_couts[i]:
+                liste_des_couts[i] = cout
                 coupures[i] = j
-    cout_total = dp[-1]
+    cout_total = liste_des_couts[-1]
 
     # Reconstruit le texte à partir du dictionnaire des coupures
     texte_justifie = reconstruire_texte(liste_mots, coupures)
